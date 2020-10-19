dc26a13fcb84cc6a81f9023de29b9a655483d1d0
MEFM <mynorflorian1998@gmail.com>
2020-10-19 02:15:06 -0600

    Comenznado consulta y union api/front

--- BackEnd/src/routes/routes.js
+++ BackEnd/src/routes/routes.js
@@ -51,6 +51,119 @@ router.post('/addUser', async (req, res) => {
     })
 })
 
+
+router.get('/login',async (req, res)=>{
+    sql = "";
+
+
+})
+
+router.post('/login/recuperarCon', async (req, res) =>{})
+
+router.put('/login/recuperarCon/canbiarCont', async (req, res)=>{})
+
+router.post('/crearUsuario', async(req, res) =>{})
+
+router.get('/perfil', async(req, res) =>{}) //basicamente get user
+
+router.put('/perfil/editar',async (req, res) =>{})
+
+router.post('perfil/agregarProd', async (req, res) =>{})
+
+//Area de publicacion
+
+router.post('/like', async (req, res) =>{
+
+})
+
+router.get('/comentarios', async (req, res) =>{})
+
+router.post('/comentarios/subirComentario', async(req, res) =>{})
+
+router.get('/mostrarProd', async(req, res) =>{
+    //Esta sera consulta para el area de detalle
+})
+
+router.post('/denunciar', async(req, res) =>{})
+
+router.post('/chat/crearSala', async (req, res)=>{
+
+})
+
+router.post('/chat/crearSalaProd', async (req, res) =>{})
+
+router.post('/chat/enviarMensaje', async(req, res) =>{})
+
+router.get('/chat/conversacion', async(req, res)=>{})
+//Area de carrito de comrpas
+
+router.get('/getCart', async (req, res)=>{
+
+})
+
+router.get('/addToCart', async(req,res)=>{
+
+})
+
+//De add to cart a confirmar compra va casi igual
+
+router.get('/confirmarCompra', async (req, res) =>{
+
+})
+
+
+//Area de busqueda y respuesta
+
+router.get('/buscarPorCategoria', async (req, res) =>{
+
+})
+
+router.get('/buscarPalabrasClave', async (req, res)=>{
+
+})
+
+router.get('/buscarProd', async(req, res)=>{
+
+})
+
+//administracion
+
+router.get('/consulta1', async(req, res)=>{
+
+})
+
+router.get('/consulta2', async(req, res)=>{
+    
+})
+
+router.get('/consulta3', async(req, res)=>{
+    
+})
+
+router.get('/consulta4', async(req, res)=>{
+    
+})
+
+router.get('/consulta5', async(req, res)=>{
+    
+})
+
+router.get('/consulta6', async(req, res)=>{
+    
+})
+
+router.get('/consulta7', async(req, res)=>{
+    
+})
+
+router.get('/consulta8', async(req, res)=>{
+    
+})
+
+router.get('/denuncias', async(req, res) =>{})
+
+router.get('/denuncias/desicion', async(req,res)=>{})
+
 /**
  * 
 cod_cliente int not null,
